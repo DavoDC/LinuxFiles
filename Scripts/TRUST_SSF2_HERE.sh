3fd0144866bfa8409279dde21d5c2c05f58a8b23
David <skywalker_dc@live.com.au>
2021-08-07 09:59:33 +0800

    Added SSF2 script

---
+++ Scripts/TRUST_SSF2_HERE.sh
@@ -0,0 +1,13 @@
+
+#exit
+
+# Make settings file directory
+#mkdir -p "/home/david/.macromedia/Flash_Player/#Security/FlashPlayerTrust"
+
+# Go to settings file directory
+
+# Make settings file
+#SSF2.cfg
+
+#/home/david/Downloads/FOLDER1/SSF2/data
+
