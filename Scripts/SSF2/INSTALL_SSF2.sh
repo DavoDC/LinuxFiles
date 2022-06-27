bc8060dedd068ac048f9df5b699a40e0ef85a727
David <45155292+DavoDC@users.noreply.github.com>
2022-06-27 11:14:42 +0800

    SSF2 Install Script - made progress

--- Scripts/SSF2/INSTALL_SSF2.sh
+++ Scripts/SSF2/INSTALL_SSF2.sh
@@ -1,71 +1,189 @@
 #!/bin/bash
 
-### SCRIPT THAT LETS SSF2 RUN ON LINUX ###
+### SCRIPT FOR INSTALLING SSF2 ON LINUX ###
 ###
-### Brief Description: Sets the SSF2 data folder as a Trusted Location for Adobe Flash Player
+### Brief Description: Install a Linux SSF2 version of the user's choosing
 ###
 ### Author: davo#1776
 ###
-### First Created: 07/08/21
+### First Created: 27/06/22
 ###
-### Tested On: Ubuntu 20.04.2.0 LTS (Focal Fossa), Manjaro 
+### Tested On: Ubuntu 22.04 LTS (Jammy Jellyfish)
 ###
 ### Usage:
 ### 1. Download this script
-### - a) Open this link: https://github.com/DavoDC/LinuxFiles/raw/main/Scripts/TRUST_SSF2_HERE.sh
+### - a) Open this link: https://github.com/DavoDC/LinuxFiles/blob/main/Scripts/SSF2/INSTALL_SSF2.sh
 ### - b) Save the file as a '.sh' (right click and select 'Save as' or use Ctrl+S)
-### 2. In your file explorer, open the folder where you have extracted the Linux version of SSF2 
-### - In this folder you should see the SSF2 executable and the 'data' folder
-### 3. Put the script in that folder, next to the executable 
-### 4. Right click and select "Open in Terminal", to open your terminal to the right folder
-### 5. Enable the script using: "chmod u+x TRUST_SSF2_HERE.sh"
-### 6. Run the script using the command: "./TRUST_SSF2_HERE.sh", and close the terminal
-### 7. Restart SSF2 and it will load beyond 5%!!!
+### 2. Find the folder where you saved the script
+### 3. Right click and select "Open in Terminal", to open your terminal to the right folder
+### 5. Enable the script using: "chmod u+x INSTALL_SSF2.sh"
+### 6. Run the script using: "./INSTALL_SSF2.sh"
+### 7. Follow the prompts
 ###
-### Usage Notes:
-### - If you move your SSF2 installation to a new directory, you will need to re-run the script there
-### - The script supports having multiple installations of SSF2 in different folders that are trusted
-### - If the game updates and your SSF2 directory path changes, you will need to re-run the script
+### This is a 'scriptified' version of the steps from the Player Guide.
+### https://docs.google.com/document/d/1l5VrAaWmLozu9qnwdjz6MGA9GyurlkgNF8t72eZ4-54/edit#heading=h.cmlrz1iib8bd
 ###
-### Detailed Description (The Purpose/Background of this Script):
-### - When you download, extract and execute the native Linux version of SSF2, it doesn't load.
-### - The game gets stuck at 5% in the loading screen, because it cannot access the game's data files.
-### - These data files (e.g. DAT0.ssf, DAT1.ssf etc.) are in the 'data' folder next to the executable
-### - Linux Adobe Flash Player (AFP) will only load from folders that are "Trusted Locations"
-### - Since the 'data' folder hasn't been set as "Trusted", the game cannot load the required files
-### - To make a folder "Trusted", Adobe advises you to right click on the AFP window and select Global Settings
-### - Clicking this sends you to webpage with an online settings manager that runs on Flash
-### - This settings manager can set folders as "Trusted", but since its runs on Flash you can't really run it
-### - You cannot run it as Flash has been removed from everywhere as it has reached its end of life
-### - Thus you cannot access the settings manager, which leads to SSF2 not loading
-### - What this script does is create the same setting file that the settings manager would've created
-### - It sets the data folder next to where it is ran as "Trusted", letting SSF2 load
-### - So this script is a workaround/alternative to using Adobe's Global Settings Manager
 
-# Store the Adobe Flash Player setting file directory path
-SETTINGS_DIR=~/.macromedia/Flash_Player/'#Security'/FlashPlayerTrust
 
-# Make settings file directory folders (or do nothing if it exists already)
-mkdir -p $SETTINGS_DIR
 
-# Retrieve the directory in which this script is running
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# Determine the SSF2 Data folder path (assuming the script is being ran next to it)
-DATA_DIR=$SCRIPT_DIR/data
+##### HELPER FUNCTIONS
 
-# Create settings file (or append to it)
-echo $DATA_DIR >> $SETTINGS_DIR/SSF2.cfg
+### Helper Function: isNotInstalled()
+# Given a package name, return true if NOT installed and false otherwise.
+# Argument 1: Package Name
+function isNotInstalled() {
 
+	# Check if installed
+	check=$(dpkg-query -W --showformat='${Status}\n' $1|grep "install ok installed")
+
+	## Convert check to numerical value
+	# If check came back as not installed
+	if [ "" = "$check" ]; then
+
+		# Return true (not installed)
+		return 0;
+	else
+
+		# Return false (installed)
+		return 1;
+	fi
+}
+
+### Helper Function: install()
+# Given a package name, install if it isn't installed
+# Argument 1: Package Name
+function install() {
+
+	# Notify
+	echo "Package: $1"
+	
+	# If not installed
+	if isNotInstalled $1; then
+
+		# Notify
+		echo "Not installed"
+
+		# Install it unattended
+		sudo apt install $1 -y
+	else 
+		# Else if it is installed, notify
+		echo "Already installed"
+	fi
+}
+
+
+
+
+
+
+
+
+##### ACTUAL SCRIPT
 # Notify user
-echo "RUNNING LINUX SSF2 FIX - by davo#1776"
+echo "#### INSTALL SSF2 ON LINUX ### - by davo#1776"
 echo ""
-echo "Settings file created/updated:"
-echo "'$SETTINGS_DIR/SSF2.cfg'"
-echo ""
-echo "This directory has been added as a Trusted Location for Adobe Flash Player:"
-echo "'$DATA_DIR'"
-echo ""
-echo "This copy of SSF2 has been fixed successfully! Enjoy playing SSF2 on Linux!"
-echo ""
+
+
+# Ask user which type is wanted
+# If you are not sure, choose WIne Installer
+# Can check guide to help you decide
+# https://docs.google.com/document/d/1l5VrAaWmLozu9qnwdjz6MGA9GyurlkgNF8t72eZ4-54/edit#heading=h.cmlrz1iib8bd
+
+
+
+# Make games folder and let user know
+# put in variable.  hardcode for now
+#cd ~/Games/SSF2
+
+
+## DOWNLOAD FILE
+#use gensetup function
+#install "wget"
+#echo ""
+#wget https://www.supersmashflash.com/play/ssf2/downloads/
+#cat dwl.html | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u | grep "https://cdn.supersmashflash.com/ssf2/downloads"
+#grep by chosen version? 
+## download URL/file
+
+
+##### Native 
+## 1. extract
+#install "tar"
+#
+#tar -xf SSF2BetaLinux.*.tar --one-top-level
+## 2. enter folder created 
+# cd SSF2BetaLinux.*/
+## 3. run fix script
+#chmod u+x trust-ssf2.sh && ./trust-ssf2.sh
+## 4. you can add this to your bashrc so you can start ssf2 from anywhere
+# function ssf2 {
+        # cd ~/Downloads/SSF2_Folder
+        # ./SSF2
+# }
+#For the Native Linux version, there is no replay autosave option in the Data menu.
+
+
+
+##### Wine 
+# 1. pre reqs
+## Enable 32-bit packages with the command: 
+#sudo dpkg --add-architecture i386
+## Update package lists with new 32 bit packages with: 
+#sudo apt update
+## Install wine and wine32 with: 
+#sudo apt install wine wine32 winbind -y
+## Install SSF2 packages
+# Enable 32 bit packages and update package list
+# sudo dpkg --add-architecture i386
+# echo ""
+# sudo apt update -y
+# echo ""
+
+#Install wine
+# install "wine"
+# echo ""
+# install "wine32"
+# echo ""
+# install "winbind"
+# echo ""
+
+
+
+
+##### Wine Installer
+# Notify: An installer window will appear. Follow the prompts.
+# 2. open with wine
+# wine SSF2BetaSetup.32bit.*.exe
+
+# Print
+#To run the game on Ubuntu:
+#Press your Windows key to search then type “Super”. SSF2 should come up as it is now in your list of applications. Just click on it to start the game.
+#You can also right-click on the app after finding it in search and add it to favourites for easier future access.
+
+
+
+
+
+##### Wine Portable
+
+## Extract
+#sudo apt-get install unzip -y
+# unzip -d WINE_SSF2_PORTABLE SSF2BetaWindows.32bit.*.portable.zip
+
+#cd WINE_SSF2_PORTABLE/*
+#wine SSF2.exe
+
+## 4. you can add this to your bashrc so you can start ssf2 from anywhere
+# function wine_ssf2 {
+        # cd ~/Downloads/SSF2_Folder
+        # wine SSF2.exe
+# }
+
+#For the Wine Linux version (Installer and Portable), the replay folder path will be something like:
+#/home/david/.wine/drive_c/users/david/SSF2Replays
+
+
+
+
 
