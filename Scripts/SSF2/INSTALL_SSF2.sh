b8d9a64d06af7405bcdf2e008b41a1dd4a16bc77
David C <45155292+DavoDC@users.noreply.github.com>
2022-06-27 22:58:21 +0800

    SSF2 Install Script: Finished!

--- Scripts/SSF2/INSTALL_SSF2.sh
+++ Scripts/SSF2/INSTALL_SSF2.sh
@@ -12,8 +12,9 @@
 ###
 ### Usage:
 ### 1. Open the folder where you want SSF2 to be installed with your File Manager GUI.
+####  - Note: A folder named 'SSF2' will be created here
 ### 2. Right click in it and select "Open in Terminal".
-### 3. Paste in this command to get the script: "wget  https://github.com/DavoDC/LinuxFiles/raw/main/Scripts/SSF2/INSTALL_SSF2.sh"
+### 3. Paste in this command to get the script: "wget https://github.com/DavoDC/LinuxFiles/raw/main/Scripts/SSF2/INSTALL_SSF2.sh"
 ### 4. Enable the script using: "chmod u+x INSTALL_SSF2.sh"
 ### 5. Run the script using: "./INSTALL_SSF2.sh" and follow the prompts
 ###
@@ -47,6 +48,7 @@ function isNotInstalled() {
 	fi
 }
 
+
 ### Helper Function: install()
 # Given a package name, install if it isn't installed
 # Argument 1: Package Name
@@ -60,53 +62,6 @@ function install() {
 	fi
 }
 
-### Helper Function: download()
-# Downloads a given Linux SSF2 version
-function download() {
-
-	# Notify
-	echo "Downloading $version_desc Version..."
-	echo "This may take some time, please keep the terminal open..."
-	echo ""
-	
-	# Install wget if needed
-	install "wget"
-
-	# Go into folder
-	cd $installPath
-
-	## Extract download URLs
-	echo "Extracting download URLs from official site.."
-	echo ""
-
-	# Download official download page as HTML
-	wget -q https://www.supersmashflash.com/play/ssf2/downloads/ -O dwl.html 
-
-	# Holder
-	dwlURL=""
-
-	# Extract from HTML
-	if [ "$native" = true ]; then
-		dwlURL="$( cat 'dwl.html' | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u | grep "https://cdn.supersmashflash.com/ssf2/downloads" | grep "SSF2BetaLinux" )"
-	
-	elif [ "$wine_inst" = true ]; then
-		dwlURL="$( cat 'dwl.html' | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u | grep "https://cdn.supersmashflash.com/ssf2/downloads" | grep "SSF2BetaSetup.32bit.*.exe"  )"
-
-	elif [ "$wine_port" = true ]; then
-		dwlURL="$( cat 'dwl.html' | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u | grep "https://cdn.supersmashflash.com/ssf2/downloads" | grep "SSF2BetaWindows.32bit.*.portable.zip" )"
-
-	fi
-
-	# Remove HTML download
-	rm 'dwl.html'
-
-	# Download file
-	wget $dwlURL
-
-	# Space
-	echo ""
-}
-
 
 ### Helper Function: printYellow()
 # Prints a string in yellow
@@ -116,6 +71,30 @@ nc='\033[0m'
 function printYellow() {
 	printf "${yellow}$1${nc}"
 }
+
+
+### Helper Function: giveBashrcAdvice()
+# Print advice regarding bashrc functions
+# Argument 1: Run Command
+# Argument 2: Function Name
+function giveBashrcAdvice() {
+	echo ""
+	echo "Advice on running the game ($version_desc Version):"
+	echo "You have to open a terminal in the"
+	echo "'$(pwd)' folder/directory,"
+	echo "and type '$1'."
+	echo ""
+	echo "To make starting it easier, you can add a function to your '~/.bashrc' file."
+	echo "If you add this to your '~/.bashrc' file, you can start SSF2 from anywhere."
+	printYellow " function $2 {"
+    printYellow "\n    cd "$(pwd)""
+    printYellow "\n    $1"
+	printYellow "\n }"
+	echo ""
+	echo "Once this is set up, type '$2' to start the game from any terminal."
+	echo ""
+}
+
 
 
 
@@ -135,12 +114,11 @@ echo " A) Native"
 echo " B) Wine Installer"
 echo " C) Wine Portable"
 echo ""
-echo "To help you decide, check the guide:"
-guide="https://docs.google.com/document/d/1l5VrAaWmLozu9qnwdjz6MGA9GyurlkgNF8t72eZ4-54/edit#heading=h.cmlrz1iib8bd"
-printYellow $guide
+echo "To help you decide, check the comparison table in the Player Guide:"
+printYellow "https://docs.google.com/document/d/1l5VrAaWmLozu9qnwdjz6MGA9GyurlkgNF8t72eZ4-54/edit#heading=h.cmlrz1iib8bd"
 echo ""
 echo ""
-echo "If you are not sure, choose B (Wine Installer)."
+echo "If you are not sure, choose/type 'C'."
 echo ""
 echo "Type the letter of your desired version and press enter."
 echo "Choice (A or B or C):"
@@ -195,6 +173,10 @@ read -p "Press any key to continue......"
 echo ""
 
 
+### Install canberra library
+install "libcanberra-gtk-module"
+
+
 ### Setup install folder
 
 # Retrieve the directory in which this script is running
@@ -207,12 +189,49 @@ installPath=$script_dir/SSF2
 mkdir -p $installPath
 
 
-### Download version file
-download
 
+# Notify
+echo "Downloading $version_desc Version..."
+echo "This may take some time, please keep the terminal open..."
+echo ""
+	
+# Install wget if needed
+install "wget"
 
-### Install canberra library
-install "libcanberra-gtk-module"
+# Go into folder
+cd $installPath
+
+## Extract download URLs
+echo "Extracting download URLs from official site.."
+echo ""
+
+# Download official download page as HTML
+wget -q https://www.supersmashflash.com/play/ssf2/downloads/ -O dwl.html 
+
+# Holder
+dwlURL=""
+
+# Extract from HTML
+if [ "$native" = true ]; then
+	dwlURL="$( cat 'dwl.html' | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u | grep "https://cdn.supersmashflash.com/ssf2/downloads" | grep "SSF2BetaLinux" )"
+
+elif [ "$wine_inst" = true ]; then
+	dwlURL="$( cat 'dwl.html' | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u | grep "https://cdn.supersmashflash.com/ssf2/downloads" | grep "SSF2BetaSetup.32bit.*.exe"  )"
+
+elif [ "$wine_port" = true ]; then
+	dwlURL="$( cat 'dwl.html' | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u | grep "https://cdn.supersmashflash.com/ssf2/downloads" | grep "SSF2BetaWindows.32bit.*.portable.zip" )"
+
+fi
+
+# Remove HTML download
+rm 'dwl.html'
+
+# Download chosen SSF2 version file
+wget $dwlURL
+
+# Space
+echo ""
+
 
 
 
@@ -237,20 +256,12 @@ if [ "$native" = true ]; then
 	chmod u+x trust-ssf2.sh && ./trust-ssf2.sh
 	echo ""
 
-	# Give bashrc function tip
-	nativePath="$(pwd)"
-	echo "Tip 1:"
-	echo "If you add this to your bashrc file,"
-	echo "you can start ssf2 from anywhere."
-	printYellow " function ssf2 {"
-    printYellow "\n    cd $nativePath"
-    printYellow "\n    ./SSF2"
-	printYellow "\n }"
-	echo ""
-	echo ""
+	# Print advice regarding bashrc functions
+	# Argument 1: Run Command
+	# Argument 2: Function Name
+ 	giveBashrcAdvice "./SSF2" "native_ssf2"
 
 	# Give replay folder tip
-	echo "Tip 2:"
 	echo "For the Native Linux version,"
 	echo "there is no replay autosave option in the Data menu."
 	echo "Thus there is no autosaved replay folder for this version."
@@ -259,35 +270,23 @@ fi
 
 
 
-
-
 ##### Wine
 if [ "$wine_inst" = true  ] || [ "$wine_port" = true  ]; then
     echo "Installing Wine..."
 	echo ""
+
+	# Enable 32-bit packages
+	sudo dpkg --add-architecture i386
+
+	# Update package lists with new 32 bit packages (quietly)
+	sudo apt update > /dev/null 2>&1
+
+	# Install wine libraries
+    install "wine"
+	install "wine32"
+	install "winbind"
+
 fi
-
-# 1. pre reqs
-## Enable 32-bit packages with the command: 
-#sudo dpkg --add-architecture i386
-## Update package lists with new 32 bit packages with: 
-#sudo apt update
-## Install wine and wine32 with: 
-#sudo apt install wine wine32 winbind -y
-## Install SSF2 packages
-# Enable 32 bit packages and update package list
-# sudo dpkg --add-architecture i386
-# echo ""
-# sudo apt update -y
-# echo ""
-
-#Install wine
-# install "wine"
-# echo ""
-# install "wine32"
-# echo ""
-# install "winbind"
-# echo ""
 
 
 
@@ -295,44 +294,69 @@ fi
 ##### Install Wine Installer Version
 if [ "$wine_inst" = true ]; then
 
-	echo "Installing wine installer version"
+	# Notify
+	echo "An installer window will soon appear."
+	echo "You will need to follow the prompts."
+	read -p "Press any key when you're ready....."
 
+	# Open Windows installer with Wine
+	wine SSF2BetaSetup.32bit.*.exe
+
+	# Give advice
+	echo ""
+	echo ""
+	echo "Advice on making the game easier to start on Ubuntu:"
+	echo "Press your Windows key to search then type 'Super'."
+	echo "SSF2 should come up as it is now in your list of applications."
+	echo "Just click on it to start the game."
+	echo ""
+	echo "You can also right-click on the app after finding it in search,"
+	echo "and add it to favourites for easier future access."
+	echo ""
+	echo "You can also copy the '.desktop' file to your desktop,"
+	echo "right click on it, and enable/allow launching,"
+	echo "to get a desktop shortcut."
+	echo ""
 fi
-# Notify: An installer window will appear. Follow the prompts.
-# 2. open with wine
-# wine SSF2BetaSetup.32bit.*.exe
-
-# Print
-#To run the game on Ubuntu:
-#Press your Windows key to search then type “Super”. SSF2 should come up as it is now in your list of applications. Just click on it to start the game.
-#You can also right-click on the app after finding it in search and add it to favourites for easier future access.
 
 
 
-
-
-##### Wine Portable
+##### Install Wine Portable Version
 if [ "$wine_port" = true ]; then
 
-	echo "Installing wine portable version"
+	# Extract
+	echo "Extracting downloaded archive..."
+	echo ""
+	install "unzip"
+	unzip -qq SSF2BetaWindows.32bit.*.portable.zip
+
+	# Go into created folder
+	cd SSF2BetaWindows.32bit.*.portable
+
+	# Print advice regarding bashrc functions
+	# Argument 1: Run Command
+	# Argument 2: Function Name
+ 	giveBashrcAdvice "wine SSF2.exe" "wine_ssf2"
 
 fi
 
-## Extract
-#sudo apt-get install unzip -y
-# unzip -d WINE_SSF2_PORTABLE SSF2BetaWindows.32bit.*.portable.zip
 
-#cd WINE_SSF2_PORTABLE/*
-#wine SSF2.exe
 
-## 4. you can add this to your bashrc so you can start ssf2 from anywhere
-# function wine_ssf2 {
-        # cd ~/Downloads/SSF2_Folder
-        # wine SSF2.exe
-# }
 
-#For the Wine Linux version (Installer and Portable), the replay folder path will be something like:
-#/home/david/.wine/drive_c/users/david/SSF2Replays
+
+
+
+
+##### Wine Autosaved Replay Path Tip
+if [ "$wine_inst" = true  ] || [ "$wine_port" = true  ]; then
+    echo ""
+	echo "Note about Autosaved Replay Path:"
+	echo "For both wine versions (Installer and Portable),"
+	echo "the autosaved replay folder path will be something like:"
+	echo "/home/$USER/.wine/drive_c/users/$USER/SSF2Replays"
+fi
+
+
 
 
 
@@ -342,6 +366,10 @@ echo ""
 echo ""
 echo ""
 echo "FINISHED!"
+echo ""
+echo "If you have any issues, check the Linux Troubleshooting section in the Player Guide:"
+printYellow "https://docs.google.com/document/d/1l5VrAaWmLozu9qnwdjz6MGA9GyurlkgNF8t72eZ4-54/edit#heading=h.3i7wifh0e5nc"
+echo ""
 echo ""
 echo "Enjoy playing SSF2 on Linux!"
 echo ""
